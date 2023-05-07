0cde6642c5ed9d0a39c3ef3b60be37822b57321d
Korissa. RisakoSuzuki <okasi_aru570@icloud.com>
2023-05-07 16:54:01 +0900

    Initial Commit

---
+++ Gatya/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gatya
+//
+//  Created by 鈴木理紗子 on 2023/05/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
